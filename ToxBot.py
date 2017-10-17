74b04fd08f3d4df028df6231157c735ae52d22fb
Andrew Litteken <andrew.litteken@gmail.com>
2017-10-17 10:39:08 -0500

    Translation keys

--- ToxBot.py
+++ ToxBot.py
@@ -153,7 +153,7 @@ class ToxBot:
         tone_analyzer = ToneAnalyzer('15995331-bf1f-4ca7-9a79-4b1ae9c5d544', 'hnioI4YAvFbn', '2016-05-19',
                                      "./data/all_marked_data.txt", "./data/all_marked_data_scores.txt", False)
         personality_analyzer = PersonalityAnalyzer('d68b1f5b-c43d-4b0d-a644-775e1071194e', 'fJMpsSvXYBIF', '2016-9-20')
-        language_analyzer = TranslationModule('15995331-bf1f-4ca7-9a79-4b1ae9c5d544', 'hnioI4YAvFbn')
+        language_analyzer = TranslationModule('5a26e50e-a391-451f-9cbe-830f84052ad7', 'dQmWGtOGWeFV')
 
         tone_analyzer_thread = threading.Thread(target=self.analyze_tone, args=(tone_analyzer,))
         jeffy_thread = threading.Thread(target=self.jeffy_listen, args=("johnathonnow", "oauth:mm84kpr5or9rmashwlp9f8dxprqm3b", "irc.chat.twitch.tv", "#" + str(channel)))
